6b6a3ef5f043f03f32afe84966a974cad75b65e7
wajeht <zombyard@gmail.com>
2020-03-26 16:34:26 -0500

    initial commit

---
+++ Powerlifting Attempt Selectoin Calculator/ResultViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultViewController.swift
+//  Powerlifting Attempt Selectoin Calculator
+//
+//  Created by Ko Nyein on 3/26/20.
+//  Copyright © 2020 Kyaw Nyein. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Powerlifting Attempt Selectoin Calculator/ViewController.swift
+++ Powerlifting Attempt Selectoin Calculator/ViewController.swift
@@ -10,11 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblMale: UIButton!
+    @IBOutlet weak var lblFemale: UIButton!
+    @IBOutlet weak var lblCalculate: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.lblCalculate.layer.cornerRadius = 10
+
+        
     }
+    
+    var isClicked:Bool = false
+    
+    @IBAction func btnMale(_ sender: Any) {
+        
+        if isClicked {
+            isClicked = false
+            self.lblMale.backgroundColor = UIColor.red
+            self.lblMale.layer.cornerRadius = 10
+            self.lblMale.titleLabel?.textColor = UIColor.white
+        }
+        else {
+            isClicked = true
+            self.lblMale.backgroundColor = UIColor.white
+            self.lblMale.layer.cornerRadius = 10
+            self.lblMale.titleLabel?.textColor = UIColor.black
+        }
+    }
+    
+    @IBAction func btnFemale(_ sender: Any) {
 
-
+        
+        if isClicked {
+            isClicked = false
+            self.lblFemale.backgroundColor = UIColor.red
+            self.lblFemale.layer.cornerRadius = 10
+            self.lblFemale.titleLabel?.textColor = UIColor.white
+        }
+        else {
+            isClicked = true
+            self.lblFemale.backgroundColor = UIColor.white
+            self.lblFemale.layer.cornerRadius = 10
+            self.lblFemale.titleLabel?.textColor = UIColor.black
+        }
+        
+        
+    }
+    
+    @IBAction func btnCalculate(_ sender: Any) {
+        
+        print("calculate")
+    }
+    
 }
 
