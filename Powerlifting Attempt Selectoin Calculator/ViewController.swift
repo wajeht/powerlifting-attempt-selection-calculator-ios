b4569b3353a2755a718109d7bbda39e488d109e7
wajeht <zombyard@gmail.com>
2020-03-25 23:31:21 -0500

    Initial Commit

---
+++ Powerlifting Attempt Selectoin Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Powerlifting Attempt Selectoin Calculator
+//
+//  Created by Ko Nyein on 3/25/20.
+//  Copyright © 2020 Kyaw Nyein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
