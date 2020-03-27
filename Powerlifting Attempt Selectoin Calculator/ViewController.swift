92369649dde65e6538c1120d267ef1c780f68bc1
wajeht <zombyard@gmail.com>
2020-03-26 21:42:48 -0500

    Fully working radio button for selecting gender

--- Powerlifting Attempt Selectoin Calculator/ViewController.swift
+++ Powerlifting Attempt Selectoin Calculator/ViewController.swift
@@ -38,47 +38,30 @@ class ViewController: UIViewController {
         }
     }
     // shift keyboard upword on textviw ends
-
-    var isClicked:Bool = false
-    
+     
     @IBAction func btnMale(_ sender: Any) {
         
-        if isClicked {
-            isClicked = false
-            self.lblMale.backgroundColor = UIColor.red
-            self.lblMale.layer.cornerRadius = 10
-            self.lblMale.titleLabel?.textColor = UIColor.white
+        if lblFemale.isSelected {
+            lblFemale.isSelected = false
+            lblMale.isSelected = true
+        } else {
+            lblMale.isSelected = true
         }
-        else {
-            isClicked = true
-            self.lblMale.backgroundColor = UIColor.white
-            self.lblMale.layer.cornerRadius = 10
-            self.lblMale.titleLabel?.textColor = UIColor.black
+    }
+
+    @IBAction func btnFemale(_ sender: Any) {
+        
+        
+        if lblMale.isSelected {
+            lblMale.isSelected = false
+            lblFemale.isSelected = true
+        } else {
+            lblFemale.isSelected = true
         }
     }
     
-    @IBAction func btnFemale(_ sender: Any) {
-
-        
-        if isClicked {
-            isClicked = false
-            self.lblFemale.backgroundColor = UIColor.red
-            self.lblFemale.layer.cornerRadius = 10
-            self.lblFemale.titleLabel?.textColor = UIColor.white
-        }
-        else {
-            isClicked = true
-            self.lblFemale.backgroundColor = UIColor.white
-            self.lblFemale.layer.cornerRadius = 10
-            self.lblFemale.titleLabel?.textColor = UIColor.black
-        }
-        
-        
-    }
     
     @IBAction func btnCalculate(_ sender: Any) {
-        print("calculate")
-
         print("calculate")
     }
     
